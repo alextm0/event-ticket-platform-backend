65c313baa265c2a7149d3d47f4cdedf243c5e15e
Nanu25 <alexandrugrancea25@gmail.com>
2025-11-08 13:39:51 +0200

    add errors

---
+++ src/main/java/com/project/event_ticket_platform/exceptions/InsufficientTicketsException.java
@@ -0,0 +1,9 @@
+package com.project.event_ticket_platform.exceptions;
+
+public class InsufficientTicketsException extends RuntimeException {
+
+	public InsufficientTicketsException(String message) {
+		super(message);
+	}
+}
+
