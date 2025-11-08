b87e2da305b41e2d061d16ce47a7668e344b82bc
Nanu25 <alexandrugrancea25@gmail.com>
2025-11-08 21:56:19 +0200

    Revert "Add exception handling and QR code generation for ticket purchases"
    This reverts commit 5c58c4d357a93c23731c82c3ca6bb22825a613ce.

--- src/main/java/com/project/event_ticket_platform/exceptions/UnauthorizedAccessException.java
+++
@@ -1,9 +0,0 @@
-package com.project.event_ticket_platform.exceptions;
-
-public class UnauthorizedAccessException extends RuntimeException {
-
-	public UnauthorizedAccessException(String message) {
-		super(message);
-	}
-}
-
